f0c8694fd8e5738e258612abbb4db9104d9d6c65
Kyuwon Jeong <jkw5033@gmail.com>
2025-12-07 19:20:51 +0900

    feat: ReentrantLock 이용해 동시성 제어
    Signed-off-by: Kyuwon Jeong <jkw5033@gmail.com>

--- src/main/kotlin/com/workspace/jvmlock/coupon/service/CouponService.kt
+++ src/main/kotlin/com/workspace/jvmlock/coupon/service/CouponService.kt
@@ -2,15 +2,29 @@ package com.workspace.jvmlock.coupon.service
 
 import com.workspace.jvmlock.coupon.entity.Coupon
 import com.workspace.jvmlock.coupon.repository.CouponRepository
+import java.util.concurrent.locks.ReentrantLock
 import org.springframework.stereotype.Service
 
 @Service
 class CouponService(
     private val repository: CouponRepository,
 ) {
+    private val lock = ReentrantLock(true)
+
     fun issue(userId: Long) {
         val coupon = Coupon(userId)
-        repository.create(coupon)
+        lock.lock()
+        try {
+            if (repository.findAll().size >= MAX_COUPON_COUNT) {
+                throw IllegalStateException("쿠폰이 모두 소진되었습니다.")
+            }
+            repository.create(coupon)
+        } finally {
+            lock.unlock()
+        }
+    }
+
+    companion object {
+        private const val MAX_COUPON_COUNT = 100
     }
 }
-
