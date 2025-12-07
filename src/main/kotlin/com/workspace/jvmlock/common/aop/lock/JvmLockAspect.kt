84a31919459d6bf88ea5ea5acebdb6345130de1e
Kyuwon Jeong <jkw5033@gmail.com>
2025-12-07 20:42:26 +0900

    feat: ReentrantReadWriteLock 이용해 동시성 제어
    Signed-off-by: Kyuwon Jeong <jkw5033@gmail.com>

--- src/main/kotlin/com/workspace/jvmlock/common/aop/lock/JvmLockAspect.kt
+++ src/main/kotlin/com/workspace/jvmlock/common/aop/lock/JvmLockAspect.kt
@@ -1,8 +1,8 @@
-package com.workspace.jvmlock.common.aop
+package com.workspace.jvmlock.common.aop.lock
 
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.locks.ReentrantLock
+import java.util.concurrent.locks.ReentrantReadWriteLock
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
@@ -12,36 +12,34 @@ import org.springframework.stereotype.Component
 @Aspect
 @Component
 class JvmLockAspect {
-    private val lock = ReentrantLock(true)
-    private val scheduler = Executors.newSingleThreadScheduledExecutor()
+    private val readWriteLock = ReentrantReadWriteLock(true)
+    private val executor = Executors.newSingleThreadExecutor()
 
     @Around("@annotation(JvmLock)")
     fun lockAround(joinPoint: ProceedingJoinPoint): Any? {
         val method = (joinPoint.signature as MethodSignature).method
         val annotation = method.getAnnotation(JvmLock::class.java)
 
+        val type = annotation.type
         val waitTime = annotation.waitTime.toLong()
         val leaseTime = annotation.leaseTime.toLong()
+
+        val lock = when (type) {
+            LockType.READ -> readWriteLock.readLock()
+            LockType.WRITE -> readWriteLock.writeLock()
+        }
 
         val acquired = lock.tryLock(waitTime, TimeUnit.SECONDS)
         if (!acquired) {
             throw RuntimeException("락 대기 시간 초과 (waitTime=${waitTime}s)")
         }
 
-        val unlockTask = scheduler.schedule({
-            if (lock.isHeldByCurrentThread) {
-                lock.unlock()
-                println("락 점유 시간 만료 (${leaseTime}s)")
-            }
-        }, leaseTime, TimeUnit.SECONDS)
-
         return try {
-            joinPoint.proceed()
+            val future = executor.submit<Any?> { joinPoint.proceed() }
+            future.get(leaseTime, TimeUnit.SECONDS)
         } finally {
-            unlockTask.cancel(false)
-            if (lock.isHeldByCurrentThread) {
-                lock.unlock()
-            }
+            lock.unlock()
+            println("락 점유 시간 만료 (${leaseTime}s, type=$type)")
         }
     }
 }
